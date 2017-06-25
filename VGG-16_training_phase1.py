6c3c32b833e980e61c864a61ec9be0d64e03b44f
Alejandro Cartas <gorayni@gmail.com>
2017-06-25 20:22:46 +0200

    VGG-16 Training completed

--- VGG-16_training_phase1.py
+++ VGG-16_training_phase1.py
@@ -7,9 +7,12 @@ from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint
 from experiments.utils import HistoryLog
+from datetime import datetime
+from time import time
 import experiments as exp
 
 import os
+
 
 def get_session(gpu_fraction=0.8):
     import tensorflow as tf
@@ -23,8 +26,10 @@ def get_session(gpu_fraction=0.8):
     else:
         return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
+
 if K.backend() == 'tensorflow':
     import keras.backend.tensorflow_backend as KTF
+
     KTF.set_session(get_session(0.8))
 
 img_width, img_height = 224, 224
@@ -33,50 +38,53 @@ seed = 42
 np.random.seed(seed)
 
 sgd = SGD(lr=0.00001, decay=0.000005, momentum=0.9, nesterov=True)
-model = exp.vgg16_first_phase_model(weights=None)
-model.load_weights('weights.VGG-16.phase_1.08.hdf5')
+model = exp.vgg16_first_phase_model()
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # prepare data augmentation configuration
-train_datagen = ImageDataGenerator(rescale=1./255,
+train_datagen = ImageDataGenerator(rescale=1. / 255,
                                    rotation_range=40,
                                    width_shift_range=0.2,
                                    height_shift_range=0.2,
                                    zoom_range=0.2,
                                    horizontal_flip=True)
 
-val_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory('data/training',
-        target_size=(img_height, img_width),
-        class_mode='categorical',
-        batch_size=1)
+                                                    target_size=(img_height, img_width),
+                                                    class_mode='categorical',
+                                                    batch_size=1)
 
 validation_generator = val_datagen.flow_from_directory(
-        'data/validation',
-        target_size=(img_height, img_width),
-        class_mode='categorical',
-        batch_size=1)
+    'data/validation',
+    target_size=(img_height, img_width),
+    class_mode='categorical',
+    batch_size=1)
 
 # checkpoint
-weights_filepath="weights.VGG-16.phase_1.{epoch:02d}.hdf5"
+weights_filepath = "weights.VGG-16.phase_1.{epoch:02d}.hdf5"
 checkpoint = ModelCheckpoint(weights_filepath, monitor='val_acc', verbose=1, save_best_only=False)
 history = HistoryLog()
 
 # fine-tune the model
 model.fit_generator(
-        train_generator,
-        steps_per_epoch=36095,
-        epochs=10,
-        callbacks=[checkpoint, history],
-        validation_data=validation_generator,
-        validation_steps=2582,
-        initial_epoch=9)
+    train_generator,
+    steps_per_epoch=36095,
+    epochs=10,
+    callbacks=[checkpoint, history],
+    validation_data=validation_generator,
+    validation_steps=2582,
+    initial_epoch=0)
 
-loss_filepath = "loss.log"
+ts = time()
+timestamp = datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+
+loss_filepath = "vgg16.phase_1.loss.{}.log".format(timestamp)
 history.log_training_loss(loss_filepath)
 
-epoch_filepath = "epoch.log"
+epoch_filepath = "vgg16.phase_1.epoch.{}.log".format(timestamp)
 history.log_epoch(epoch_filepath)
 
-K.clear_session()
+if K.backend() == 'tensorflow':
+    K.clear_session()
--- VGG-16_training_phase2.py
+++ VGG-16_training_phase2.py
@@ -7,9 +7,12 @@ from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint
 from experiments.utils import HistoryLog
+from datetime import datetime
+from time import time
 import experiments as exp
 
 import os
+
 
 def get_session(gpu_fraction=0.8):
     import tensorflow as tf
@@ -22,6 +25,7 @@ def get_session(gpu_fraction=0.8):
             gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
     else:
         return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
 
 if K.backend() == 'tensorflow':
     import keras.backend.tensorflow_backend as KTF
@@ -37,45 +41,49 @@ model = exp.vgg_16_second_phase_model(weights='weights.VGG-16.phase_1.10.hdf5')
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # prepare data augmentation configuration
-train_datagen = ImageDataGenerator(rescale=1./255,
+train_datagen = ImageDataGenerator(rescale=1. / 255,
                                    rotation_range=40,
                                    width_shift_range=0.2,
                                    height_shift_range=0.2,
                                    zoom_range=0.2,
                                    horizontal_flip=True)
 
-val_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory('data/training',
-        target_size=(img_height, img_width),
-        class_mode='categorical',
-        batch_size=1)
+                                                    target_size=(img_height, img_width),
+                                                    class_mode='categorical',
+                                                    batch_size=1)
 
 validation_generator = val_datagen.flow_from_directory(
-        'data/validation',
-        target_size=(img_height, img_width),
-        class_mode='categorical',
-        batch_size=1)
+    'data/validation',
+    target_size=(img_height, img_width),
+    class_mode='categorical',
+    batch_size=1)
 
 # checkpoint
-weights_filepath="weights.VGG-16.phase_2.{epoch:02d}.hdf5"
+weights_filepath = "weights.VGG-16.phase_2.{epoch:02d}.hdf5"
 checkpoint = ModelCheckpoint(weights_filepath, monitor='val_acc', verbose=1)
 history = HistoryLog()
 
 # fine-tune the model
 model.fit_generator(
-        train_generator,
-        steps_per_epoch=36095,
-        epochs=10,
-        callbacks=[checkpoint, history],
-        validation_data=validation_generator,
-        validation_steps=2582,
-        initial_epoch=5)
+    train_generator,
+    steps_per_epoch=36095,
+    epochs=10,
+    callbacks=[checkpoint, history],
+    validation_data=validation_generator,
+    validation_steps=2582,
+    initial_epoch=0)
 
-loss_filepath = "loss.log"
+ts = time()
+timestamp = datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+
+loss_filepath = "vgg16.phase_2.loss.{}.log".format(timestamp)
 history.log_training_loss(loss_filepath)
 
-epoch_filepath = "epoch.log"
+epoch_filepath = "vgg16.phase_2.epoch.{}.log".format(timestamp)
 history.log_epoch(epoch_filepath)
 
-# K.clear_session()
+if K.backend() == 'tensorflow':
+    K.clear_session()

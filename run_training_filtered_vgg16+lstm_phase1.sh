0b45559427772c87bebf2bbb1c2e0f119e8b730c
Alejandro Cartas <gorayni@gmail.com>
2017-06-27 16:46:01 +0200

    Training description added

--- run_training_filtered_vgg16+lstm_phase1.sh
+++ run_training_filtered_vgg16+lstm_phase1.sh
@@ -1,18 +1,19 @@
-#!/bin/bash 
-tput reset
-
+#!/bin/bash
 if [ -z "$1" ]
 then
-  TIMESTEP=10  
+  TIMESTEP=10
 else
   TIMESTEP=$1
 fi
 
 if [ -z "$2" ]
 then
-  OVERLAP=2
+  OVERLAP=3
 else
   OVERLAP=$2
 fi
 
+tput reset
+
+echo "Filtered VGG-16+LSTM phase 1 training Timestep: "$TIMESTEP" Overlap: "$OVERLAP
 nohup python -u Filtered_VGG-16+LSTM_phase1_training.py --timestep $TIMESTEP --overlap $OVERLAP </dev/null 2>&1 | tee "Filtered_VGG-16+LSTM_training.timestep_"$TIMESTEP".overlap_"$OVERLAP"."`date +%Y-%m-%d_%H:%M:%S`".log" &
--- run_training_vgg16+lstm.sh
+++ run_training_vgg16+lstm.sh
@@ -1,11 +1,13 @@
-#!/bin/bash 
-tput reset
+#!/bin/bash
 
 if [ -z "$1" ]
 then
-  TIMESTEP=10  
+  TIMESTEP=10
 else
   TIMESTEP=$1
 fi
 
+tput reset
+
+echo "VGG-16+LSTM training Timestep: "$TIMESTEP
 nohup python -u VGG-16+LSTM_training.py --timestep $TIMESTEP </dev/null 2>&1 | tee "VGG-16+LSTM_training.timestep_"$TIMESTEP"."`date +%Y-%m-%d_%H:%M:%S`".log" &

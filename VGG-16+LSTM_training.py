05bc2bfcf63fb4ee819f40668739d17e6bb5dedb
Alejandro Cartas <gorayni@gmail.com>
2017-07-01 20:17:33 +0200

    VGG16+LSTM timestep 10 results added

--- VGG-16+LSTM_training.py
+++ VGG-16+LSTM_training.py
@@ -111,6 +111,6 @@ if __name__ == '__main__':
 
     if K.backend() == 'tensorflow':
       import keras.backend.tensorflow_backend as KTF
-      KTF.set_session(get_session(0.3))
+      KTF.set_session(get_session())
 
     train_net(args.timestep)
